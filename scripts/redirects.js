dc0448f257cb7032196f0ab151019192dd6b80e7
lucicek <mo.vitek@seznam.cz>
2026-01-09 19:11:01 +0100

    Add files via upload

---
+++ scripts/redirects.js
@@ -0,0 +1,23 @@
+window.AUDIO_BASE_PATH = window.AUDIO_BASE_PATH || '../';
+
+function playAudio(name) {
+  const audio = new Audio();
+  audio.src = `${window.AUDIO_BASE_PATH}audio/${name}.mp3`;
+  audio.play();
+}
+
+document.body.addEventListener('keydown', (event) => {
+  if (event.key === '1') {
+    window.location.href = `${window.AUDIO_BASE_PATH}`;
+  } else if (event.key === '2') {
+    window.location.href = `${window.AUDIO_BASE_PATH}lov`;
+  } else if (event.key === '3') {
+    window.location.href = `${window.AUDIO_BASE_PATH}stvanice`;
+  } else if (event.key === '+') {
+    playAudio('lovec-prichazi');
+  } else if (event.key === 'ě') {
+    playAudio('lovec-start');
+  } else if (event.key === 'š') {
+    playAudio('zacatek');
+  }
+});
